dab3d0b3f91d676088ed737e31104887b4842085
Nick <aa01912001@gmail.com>
2026-01-10 10:43:55 +0800

    add python solution for No.712

---
+++ No_712/Minimum ASCII Delete Sum for Two Strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        n1, n2 = len(s1), len(s2)
+        dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + ord(s1[i-1])
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        common = dp[n1][n2]
+        return sum(map(ord, s1)) + sum(map(ord, s2)) - 2 * common
