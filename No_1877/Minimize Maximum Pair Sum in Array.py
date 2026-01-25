078a3b2fadd4746107c8c86cbd3a55e982adc025
Nick <aa01912001@gmail.com>
2026-01-25 13:10:05 +0800

    add python solution for No.1877

---
+++ No_1877/Minimize Maximum Pair Sum in Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.sort()
+        ans = 0
+        for i in range(n // 2):
+            ans = max(ans, nums[i] + nums[n-i-1])
+        
+        return ans
